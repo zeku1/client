0d098c08a39ab21a80df3d8049b7ca557098710f
Ezekiel Kim Lucena <zekielucena@gmail.com>
2024-08-08 23:27:18 +0800

    tailwind light and dark theme ready

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,45 @@
 import type { Config } from "tailwindcss";
+import {createThemes } from "tw-colors";
+import colors from "tailwindcss/colors";
+
+const baseColors = [
+  "gray",
+  "red",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "purple",
+  "pink"
+];
+
+const shadeMapping = {
+  "50": "900",
+  "100": "800",
+  "200": "700",
+  "300": "600",
+  "400": "500",
+  "500": "400",
+  "600": "300",
+  "700": "200",
+  "800": "100",
+  "900": "50",
+};
+
+const generateThemeObject = (colors: any, mapping: any, invert = false) => {
+  const theme: any = {};
+  baseColors.forEach((color) => {
+    theme[color] = {};
+    Object.entries(mapping).forEach(([key,value]: any) => {
+      const shadeKey = invert ? value : key;
+      theme[color][key] = colors[color][shadeKey];
+    });
+  });
+  return theme;
+}
+
+const lightTheme = generateThemeObject(colors, shadeMapping);
+const darkTheme = generateThemeObject(colors, shadeMapping, true);
 
 const config: Config = {
   content: [
@@ -17,4 +58,5 @@ const config: Config = {
   },
   plugins: [],
 };
+
 export default config;
