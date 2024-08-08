584158adddc010a0acebd552b2749ec01d42b028
Ezekiel Kim Lucena <zekielucena@gmail.com>
2024-08-09 03:59:12 +0800

    Added sidebar and navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
+import Dashboard from "@/app/dashboard/page";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello world!!!
-      maayong buntag sa tanan!!
-    </main>
+
+    <Dashboard />
+    
   );
 }
